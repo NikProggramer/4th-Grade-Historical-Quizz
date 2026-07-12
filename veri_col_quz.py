98e54626bf39a9ef3900a710e62087583980b328
NikProggramer <nikcc1814@gmail.com>
2026-07-12 19:45:54 +0300

    Update veri_col_quz.py

--- veri_col_quz.py
+++ veri_col_quz.py
@@ -19,96 +19,94 @@ st.title("4th Grade Historical Quizz")
 
 questions = [
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images/image_1.png",
+        "image": "images/image_1.png",
         "question": "What was the biggest country in Europe in the 1950's?",
         "options": ["East Germany", "Romania", "Vatican", "USSR"],
         "answer": "USSR"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_2.png",
+        "image": "images/image_2.png",
         "question": "Who is the real roman?",
         "options": ["USA", "Germany", "Russia", "Greece and Italy"],
         "answer": "Greece and Italy"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_3.png",
+        "image": "images/image_3.png",
         "question": "Who conquered Dacia?",
-        "options": ["Egypt", "Roman Empire","USSR", "Canada"],
+        "options": ["Egypt", "Roman Empire", "USSR", "Canada"],
         "answer": "Roman Empire"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_4.png",
-        "question" : "When did WW2 end?",
-        "options" : ["1980", "1945", "2026"],
-        "answer" : "1945"
+        "image": "images/image_4.png",
+        "question": "When did WW2 end?",
+        "options": ["1980", "1945", "2026"],
+        "answer": "1945"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_5.png",
-        "question" : "What was the biggest empire?",
-        "options" : ["Dacia", "British Empire", "Mongolian Empire"],
-        "answer" : "British Empire"
+        "image": "images/image_5.png",
+        "question": "What was the biggest empire?",
+        "options": ["Dacia", "British Empire", "Mongolian Empire"],
+        "answer": "British Empire"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_6.png",
-        "question" : "Who did the soviets fight in the cold war?",
-        "options" : ["Russia", "NATO", "Moldova"],
-        "answer" : "NATO"
+        "image": "images/image_6.png",
+        "question": "Who did the soviets fight in the cold war?",
+        "options": ["Russia", "NATO", "Moldova"],
+        "answer": "NATO"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_7.jpg",
-        "question" : "What Ideology in history involved Kings and queens?",
-        "options" : ["Communism", "Monarchy", "Democracy"],
-        "answer" : "Monarchy"
+        "image": "images/image_7.jpg",
+        "question": "What Ideology in history involved Kings and queens?",
+        "options": ["Communism", "Monarchy", "Democracy"],
+        "answer": "Monarchy"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_8.webp",
-        "question" : "Who was the founding father of the United States?",
-        "options" : ["Stalin", "George Washington", "Burebista"],
-        "answer" : "George Washington"
+        "image": "images/image_8.webp",
+        "question": "Who was the founding father of the United States?",
+        "options": ["Stalin", "George Washington", "Burebista"],
+        "answer": "George Washington"
     },
     {
-        
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_9.jpg",
-        "question" : "When did WW1 end?",
-        "options" : ["1918", "1946", "2014"],
-        "answer" : "1918"
+        "image": "images/image_9.jpg",
+        "question": "When did WW1 end?",
+        "options": ["1918", "1946", "2014"],
+        "answer": "1918"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_10.jpg",
-        "question" : "How Costly was WW2?",
-        "options" : ["Not costly", "Insanely Costly", "Mildy Costly"],
-        "answer" : "Insanely Costly"
+        "image": "images/image_10.jpg",
+        "question": "How Costly was WW2?",
+        "options": ["Not costly", "Insanely Costly", "Mildy Costly"],
+        "answer": "Insanely Costly"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_11.webp",
-        "question" : "What was the best ruler of Romania?",
-        "options" : ["Burebista", "Andrei", "Alexandru Ioan Cuza"],
-        "answer" : "Alexandru Ioan Cuza"
+        "image": "images/image_11.webp",
+        "question": "What was the best ruler of Romania?",
+        "options": ["Burebista", "Andrei", "Alexandru Ioan Cuza"],
+        "answer": "Alexandru Ioan Cuza"
     },
     {
-        
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_12.jpg",
-        "question" : "What is the motive of WW1?",
-        "options" : ["Serbian assination of Franz Ferdinand", "Attack on poland", "Russian civil war"],
-        "answer" : "Serbian assination of Franz Ferdinand"
+        "image": "images/image_12.jpg",
+        "question": "What is the motive of WW1?",
+        "options": ["Serbian assination of Franz Ferdinand", "Attack on poland", "Russian civil war"],
+        "answer": "Serbian assination of Franz Ferdinand"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_13.webp",
-        "question" : "How long did the Romanian Peoples Republic last?",
-        "options" : ["18 years", "1 hr", "2 years"],
-        "answer" : "18 years"
+        "image": "images/image_13.webp",
+        "question": "How long did the Romanian Peoples Republic last?",
+        "options": ["18 years", "1 hr", "2 years"],
+        "answer": "18 years"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_14.webp",
-        "question" : "What was the most greatest era of Germany?",
-        "options" : ["Re unfication of 1990", "The Great Depression", "WW2"],
-        "answer" : "Re unfication of 1990"
+        "image": "images/image_14.webp",
+        "question": "What was the most greatest era of Germany?",
+        "options": ["Re unfication of 1990", "The Great Depression", "WW2"],
+        "answer": "Re unfication of 1990"
     },
     {
-        "image": "https://github.com/NikProggramer/4th-Grade-Historical-Quizz/tree/main/images\image_15.webp",
-        "question" : "What was the last emperor of the Byzantine empire?",
-        "options" : ["Trajan", "Pupienius", "Constantine XI"],
-        "answer" : "Constantine XI"
+        "image": "images/image_15.webp",
+        "question": "What was the last emperor of the Byzantine empire?",
+        "options": ["Trajan", "Pupienius", "Constantine XI"],
+        "answer": "Constantine XI"
     }
 ]
 
